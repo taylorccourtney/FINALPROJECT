27a7ada7eb6a71b0def2a4aca2087fe15d5cedac
Taylor <apple@Apples-MacBook-Pro-45.local>
2019-07-31 14:26:10 -0400

    AlmostDoneText

--- LookBook/ViewController.swift
+++ LookBook/ViewController.swift
@@ -9,14 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["🚶‍♂️" : "casual", "🕺" : "formal", "🏃‍♂️" : "athletic"]
+    
+    let customMessages = ["casual":["outfit 1","outfit 2","outfit 3"],"formal":["choice 1", "choice 2", "choice 3"], "athletic":["clothes 1", "clothes 2", "clothes 3"] ]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showMessage(sender: UIButton) {
-      
+        let selectedEmotion = sender.titleLabel?.text
+        let random = Int.random(in: 0...2)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+        let alertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "TITLE", style: UIAlertAction.Style.default, handler: nil))
+    present(alertController,animated: true,completion: nil)
         
     }
 }
