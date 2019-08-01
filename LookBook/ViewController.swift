e59aa9e55e01a74e67728d578d2a33ad89bee5e4
Taylor <apple@Apples-MacBook-Pro-45.local>
2019-08-01 15:54:07 -0400

    Iteration2

--- LookBook/ViewController.swift
+++ LookBook/ViewController.swift
@@ -9,25 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let emojis = ["🚶‍♂️" : "casual", "🕺" : "formal", "🏃‍♂️" : "athletic", "🚶‍♀️" : "fcasual", "💃" : "fformal", "🏃‍♀️" : "fathletic"]
-    
-    let customMessages = ["casual":["You can never go wrong with some jeans and a plaid button up","Black/White tee goes with everything","All black outfit with colored sneakers"],"formal":["Fitted Blazers make any outfit fancy", "Button up shirts and black pants are always acceptable", "Do you have a collard shirt? If so throw on some jeans with that"], "athletic":["Sweatpants are a staple", "Muscle T-shirts allow for good ventilation", "You don't have to play basketball to wear basketball shorts to work out."]]
     
     
+    @IBOutlet var randomWord: UILabel!
+    @IBOutlet var imageOne: UIImageView!
+    @IBOutlet var imageTwo: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let imageNames = ["caone", "catwo", "cathree", "cafour", "cafive"]
+    
+    
+   
+    @IBAction func showWord(_ sender: UIButton) {
+        
+        let array = ["Don't be afraid to play with color!", "You can pair anything with jeans", "white goes with everything", "oh! black is everything!"]
+        let RandomWordGen = Int(arc4random_uniform(UInt32(array.count)))
+        randomWord.text = array[RandomWordGen]
+        
     }
-
-    @IBAction func showMessage(_ sender: UIButton) {
-        let selectedEmotion = sender.titleLabel?.text
-        let random = Int.random(in: 0...2)
-        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
-        let alertController = UIAlertController(title: "Maybe you should wear", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "continue", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController,animated: true,completion: nil)
+    
+    
+    @IBAction func showImage(_ sender: Any) {
+        
+        let leftNumber:Int = Int(arc4random_uniform(5))
+        
+        imageOne.image = UIImage(named:imageNames[leftNumber])
+        
+        let rightNumber: Int = Int(arc4random_uniform(5))
+        
+        imageTwo.image = UIImage(named:imageNames[rightNumber])
+        
+        
     }
+    
+    
+    
+    
+    
+//
+////    let emojis = ["🚶‍♂️" : "casual", "🕺" : "formal", "🏃‍♂️" : "athletic", "🚶‍♀️" : "fcasual", "💃" : "fformal", "🏃‍♀️" : "fathletic"]
+//
+//    let customMessages = ["casual":["You can never go wrong with some jeans and a plaid button up","Black/White tee goes with everything","All black outfit with colored sneakers"],"formal":["Fitted Blazers make any outfit fancy", "Button up shirts and black pants are always acceptable", "Do you have a collard shirt? If so throw on some jeans with that"], "athletic":["Sweatpants are a staple", "Muscle T-shirts allow for good ventilation", "You don't have to play basketball to wear basketball shorts to work out."]]
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    @IBAction func showMessage(_ sender: UIButton) {
+//        let selectedEmotion = sender.titleLabel?.text
+//        let random = Int.random(in: 0...2)
+//        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+//        let alertController = UIAlertController(title: "Maybe you should wear", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+//        alertController.addAction(UIAlertAction(title: "continue", style: UIAlertAction.Style.default, handler: nil))
+//        present(alertController,animated: true,completion: nil)
+//    }
+//
+//
+//
+//
+//}
 
 }
-
